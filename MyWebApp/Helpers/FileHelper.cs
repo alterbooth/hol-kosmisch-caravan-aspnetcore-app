6bba1178b5e785bb49066a9e7a1149565a97b51a
h-mitsuzono <h-mitsuzono@alterbooth.com>
2019-11-19 16:40:47 +0900

    [modify] KOS_1-552 permissions設定するよう変更

--- MyWebApp/Helpers/FileHelper.cs
+++ MyWebApp/Helpers/FileHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 
 namespace MyWebApp.Helpers
@@ -15,6 +16,9 @@ namespace MyWebApp.Helpers
             var containerName = Environment.GetEnvironmentVariable("BLOB_CONTAINER_NAME") ?? "mycontainer";
             var cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
             cloudBlobContainer.CreateAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            var permissions = cloudBlobContainer.GetPermissionsAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            permissions.PublicAccess = BlobContainerPublicAccessType.Blob;
+            cloudBlobContainer.SetPermissionsAsync(permissions).ConfigureAwait(false).GetAwaiter().GetResult();
 
             var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(file.FileName);
             cloudBlockBlob.UploadFromStreamAsync(file.OpenReadStream()).ConfigureAwait(false).GetAwaiter().GetResult();
