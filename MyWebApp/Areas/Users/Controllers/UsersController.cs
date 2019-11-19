63a22ada12ec5a054995b7ddfd08fb0fb3c96a6b
h-mitsuzono <h-mitsuzono@alterbooth.com>
2019-11-19 10:43:11 +0900

    [modify] KOS_1-552 usingの整理

--- MyWebApp/Areas/Users/Controllers/UsersController.cs
+++ MyWebApp/Areas/Users/Controllers/UsersController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Models;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using MyWebApp.Models;
 
 namespace MyWebApp.Areas.Users.Controllers
 {
